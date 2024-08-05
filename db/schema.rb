081a63c3c2f3166d1725f2fde77019a6b5c53eac
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-08-05 08:32:40 -0600

    Add Experiment migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_06_08_014930) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_05_143130) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "experiments", force: :cascade do |t|
+    t.string "name"
+    t.string "objective"
+    t.integer "num_months"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "labs", force: :cascade do |t|
     t.string "name"
