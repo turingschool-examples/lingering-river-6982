6464d844720a619e552b3a3101edbdc905cb8763
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-08-05 10:22:56 -0600

    Include sad paths for US#1 - complete tests for US#2

--- spec/features/scientists/show_spec.rb
+++ spec/features/scientists/show_spec.rb
@@ -2,23 +2,26 @@ require "rails_helper"
 
 
 RSpec.describe "Scientist Show Page", type: :feature do
-  describe "User Story #1" do
-    before :each do
-      @lab_1 = Lab.create!(name: "The Lair")
-      @lab_x = Lab.create!(name: "Rat Cage")
-      
-      @scientist_1 = Scientist.create!(name: "Dr. Jekyll", specialty: "Shape Shifting", university: "PhD in Morphology from the University of Oxford", lab: @lab_1)
-      @scientist_x = Scientist.create!(name: "Brain", specialty: "Ruminating", university: "PhD in Failed Escapes from the Maze College", lab: @lab_x)
+  before :each do
+    @lab_1 = Lab.create!(name: "The Lair")
+    @lab_x = Lab.create!(name: "Rat Cage")
+    
+    @scientist_1 = Scientist.create!(name: "Dr. Jekyll", specialty: "Shape Shifting", university: "PhD in Morphology from the University of Oxford", lab: @lab_1)
+    @scientist_2 = Scientist.create!(name: "Dr. Carl Sagan", specialty: "Astronomy", university: "PhD in Astrophysics from the University of Chicago", lab: @lab_1)
+    @scientist_x = Scientist.create!(name: "Brain", specialty: "Ruminating", university: "PhD in Failed Escapes from the Maze College", lab: @lab_x)
 
-      @experiment_1 = Experiment.create!(name: "Mr. Hyde", objective: "Change Bodies", num_months: 1)
-      @experiment_2 = Experiment.create!(name: "Antidotes", objective: "Protect From Poisoning", num_months: 3)
-      @experiment_x = Experiment.create!(name: "Gravity Waves", objective: "Float without Propulsion", num_months: 9)
+    @experiment_1 = Experiment.create!(name: "Mr. Hyde", objective: "Change Bodies", num_months: 1)
+    @experiment_2 = Experiment.create!(name: "Antidotes", objective: "Protect From Poisoning", num_months: 3)
+    @experiment_x = Experiment.create!(name: "Gravity Waves", objective: "Float without Propulsion", num_months: 9)
 
-      ScientistExperiment.create!(scientist: @scientist_1, experiment: @experiment_1)
-      ScientistExperiment.create!(scientist: @scientist_1, experiment: @experiment_2)
-      ScientistExperiment.create!(scientist: @scientist_x, experiment: @experiment_x) # For Sad Paths
-    end
-
+    ScientistExperiment.create!(scientist: @scientist_1, experiment: @experiment_1)
+    ScientistExperiment.create!(scientist: @scientist_1, experiment: @experiment_2)
+    ScientistExperiment.create!(scientist: @scientist_x, experiment: @experiment_x) # For Sad Paths
+    
+    ScientistExperiment.create!(scientist: @scientist_2, experiment: @experiment_1)
+    ScientistExperiment.create!(scientist: @scientist_2, experiment: @experiment_2)
+  end
+  context "User Story #1" do
     it "displays a scientist's information" do
       # As a visitor
       # When I visit a scientist's show page
@@ -32,8 +35,15 @@ RSpec.describe "Scientist Show Page", type: :feature do
       expect(page).to have_content("Specialty: #{@scientist_1.specialty}")
       expect(page).to have_content("University: #{@scientist_1.university}")
 
+      # Sad Path
+      expect(page).to_not have_content("Name: #{@scientist_x.name}")
+      expect(page).to_not have_content("Specialty: #{@scientist_x.specialty}")
+      expect(page).to_not have_content("University: #{@scientist_x.university}")
+
       # And I see the name of the lab where this scientist works
       expect(page).to have_content("#{@scientist_1.lab.name}")
+      
+      expect(page).to_not have_content("#{@scientist_x.lab.name}")
 
       # And I see the names of all of the experiments this scientist is running
       # expect(page).to have_content("Mr. Hyde")
@@ -45,6 +55,38 @@ RSpec.describe "Scientist Show Page", type: :feature do
       @scientist_1.experiments.each do |experiment|
         expect(page).to have_content(experiment.name)
       end
+    end
+  end
+
+  context "User Story #2" do
+    xit "removes experiment from a scientist's show page" do
+      # When I visit a scientist's show page
+      visit "/scientists/#{@scientist_1.id}"
+
+      # Then next to each experiment's name, I see a button to remove that experiment from that scientist's work load
+      expect(page).to have_content("Mr. Hyde")
+      expect(page).to have_content("Antidotes")
+
+      expect(page).to have_button "Remove Experiment"
+      expect(page).to have_link("Remove Experiment", href: "/scientists/#{@scientist_1.id}")
+
+      # When I click that button for one experiment
+      all(:button, "Remove Experiment")[0].click  # Should click the first of 2 "Remove Experiment" buttons
+      
+      # I'm brought back to the scientist's show page
+      expect(current_path).to eq("/scientists/#{@scientist_1.id}")
+      
+      # And I no longer see that experiment's name listed
+      expect(page).to_not have_content("Mr. Hyde")  # Should no longer be visible
+      expect(page).to have_content("Antidotes")   # Should remain as button was not clicked to remove
+      
+      expect(page).to have_button "Remove Experiment"  # Button belongs to "Antidotes", should remain unaffected
+
+      # And when I visit a different scientist's show page that is working on that same experiment,
+      visit "/scientists/#{@scientist_2.id}"
+      
+      # Then I see that the experiment is still on the other scientist's work load
+      expect(page).to_not have_content("Mr. Hyde")
     end
   end
 end
