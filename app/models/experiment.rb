da9670d655927d88400403bec41db2636fb0ae10
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-08-05 09:31:12 -0600

    Fix missing colons all over models and tests - model tests now passing @ 100% coverage

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -1,3 +1,4 @@
 class Experiment < ApplicationRecord
-  has_many :scientists, through: scientist_experiments
+  has_many :scientist_experiments
+  has_many :scientists, through: :scientist_experiments
 end
--- spec/models/experiment_spec.rb
+++ spec/models/experiment_spec.rb
@@ -2,5 +2,5 @@ require "rails_helper"
 
 RSpec.describe Experiment, type: :model do
   it { should have_many :scientist_experiments }
-  it { should have_many(:scientists).through(scientist_experiments) }
+  it { should have_many(:scientists).through(:scientist_experiments) }
 end
